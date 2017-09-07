a5bd8044edae037b96807f1d0e01b57890e59703
chaitu <nk00484143@techmahindra.com>
2017-09-07 17:32:55 +0530

    hello world gradle project

---
+++ src/main/java/com/techm/adms/main/Library.java
@@ -0,0 +1,10 @@
+package com.techm.adms.main;
+public class Library {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+    
+    public static void main(String[] args) {
+		System.out.println("Hello -world....");
+	}
+}
